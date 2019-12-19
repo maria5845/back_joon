ec91dbf6f3dccae5215395ece561ce2d3627dd0d
maria5845 <younglee921215@gmail.com>
2019-12-19 17:25:27 +0900

    init commit

---
+++ sol1.java
@@ -0,0 +1,19 @@
+package beakJoon;
+import java.util.Scanner;
+class sol1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num1 ;
+		int num2;
+        num1 = scanner.nextInt();
+		num2 = scanner.nextInt();
+				       
+		System.out.println(num1+num2);
+		
+		
+	}
+
+}
+
+
---
+++ sol2.java
@@ -0,0 +1,10 @@
+package beakJoon;
+
+public class sol2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
